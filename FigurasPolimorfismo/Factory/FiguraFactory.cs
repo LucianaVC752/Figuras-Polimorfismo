909f43158614073252fc122baaa8f030a41f8e7f
NadiaCiro <141196587+NadiaCiro@users.noreply.github.com>
2025-04-02 18:00:30 -0500

    Actualización de FiguraFactory para incluir nuevos tipos

---
+++ FigurasPolimorfismo/Factory/FiguraFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FigurasPolimorfismo.Factory
+{
+    public class FiguraFactory
+    {
+        public static Figura CrearFigura(int coordenadaX, int coordenadaY, Color color, int tipo) 
+        {
+            try
+            {
+                switch (tipo)
+                {
+                    case 0:
+                        return new Rectangulo(coordenadaX, coordenadaY,color);
+                    case 1:
+                        return new Circulo(coordenadaX, coordenadaY, color);
+                    case 2:
+                        return new Triangulo(coordenadaX, coordenadaY, color);
+                    case 3:
+                        return new Linea(coordenadaX, coordenadaY, color);
+                    default:
+                        return null;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                throw new ArgumentException("Ocurrió un error "+ex);
+            }
+        }
+    }
+}
